25d456f0996fd23cf0a0e469310627e7e5b5377d
FatherFoxxy <rattusunison@gmail.com>
2020-11-20 21:41:31 -0800

    Update Test.fsx to reflect new changes

--- Test.fsx
+++ Test.fsx
@@ -1,6 +1,8 @@
-#load @"..\EasyHttp\Types.fs"
-#load @"..\EasyHttp\QueryStringSerializer.fs"
-#load @"..\EasyHttp\EasyHttp.fs"
+#load @"Types.fsi" @"Types.fs"
+#load @"Serializers\Utils.fsi" @"Serializers\Utils.fs"
+#load @"Serializers\PathString.fsi" @"Serializers\PathString.fs"
+#load @"Serializers\QueryString.fsi" @"Serializers\QueryString.fs"
+#load @"EasyHttp.fsi" @"EasyHttp.fs"
 
 open System
 open EasyHttp
@@ -13,13 +15,31 @@ type Response =
         Content: string
     }
 
+type SomeOrderedData =
+    {
+        ZData: string
+        AData: string
+        QData: string
+    }
+
 type TestRecord =
     {
+        TestJson: {| someNumber: int |} -> Response
+
         [<SerializationOverride(ESerializationType.QueryString)>]
         TestQueryString: {| someNumber: int |} -> Response
 
-        [<SerializationOverride(serializationType = ESerializationType.Json)>]
-        Test: {| someNumber: int |} -> Response
+        [<SerializationOverride(ESerializationType.PathString)>]
+        [<Path("{someData}/{someNumber}{!query!}")>]
+        TestPathString: {| someData: string; someNumber: int; someQuery: string; someQuery2: string |} -> Response
+
+        [<SerializationOverride(ESerializationType.PathString)>]
+        [<Path("/some/endpoint/{!ordered!}")>]
+        TestOrderedPathString: SomeOrderedData -> Response
+
+        [<SerializationOverride(ESerializationType.PathString)>]
+        [<Path("{!ordered!}")>]
+        TestOrderedPathStringAnonRecord: {| ZData: string; AData: string |} -> Response
 
         [<Method("DELETE")>]
         TestDelete: unit -> Response
@@ -36,14 +56,26 @@ let result' =
     | Ok s -> s
     | Error err -> failwith err
 
-result'.TestQueryString {| someNumber = 1000 |}
-|> printfn "TestQueryString result:\n%A"
+result'.TestJson {| someNumber = 1000 |}
+|> printfn "Test result:\n%A\n"
 
-result'.Test {| someNumber = 1000 |}
-|> printfn "Test result:\n%A"
+result'.TestQueryString {| someNumber = 1000 |}
+|> printfn "TestQueryString result:\n%A\n"
+
+// [<Path("{someData}/{someNumber}{!query!}")>]
+result'.TestPathString {| someData = "blah"; someNumber = 32; someQuery = "queryParamValue1"; someQuery2 = "queryParamValue2" |}
+|> printfn "TestPathString result:\n%A\n"
+
+// [<Path("/some/endpoint/{!ordered!}")>]
+result'.TestOrderedPathString { ZData = "Zee"; AData = "Cool data"; QData = "Quickly qooler data" }
+|> printfn "TestOrderedPathString result:\n%A\n"
+
+// [<Path("{!ordered!}")>]
+result'.TestOrderedPathStringAnonRecord {| ZData = "Uh oh"; AData = "A data!" |}
+|> printfn "TestOrderedPathStringAnonRecord result:\n%A\n"
 
 result'.TestDelete()
-|> printfn "TestDelete result:\n%A"
+|> printfn "TestDelete result:\n%A\n"
 
 result'.UnitFunction()
-|> printfn "UnitFunction result:\n%A"
+|> printfn "UnitFunction result:\n%A\n"
